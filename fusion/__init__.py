d573b70dbd8fed0111d9702f06201044ed8a16fb
Jayakumar K <jayakumar.ethicalhacker@gmail.com>
2026-01-29 09:40:29 -0500

    Create fusion module with FusionEngine and SimpleFusionEngine
    Add multi-camera fusion module with aggregation logic.

---
+++ fusion/__init__.py
@@ -0,0 +1,8 @@
+"""
+Multi-camera fusion module
+Central aggregation and decision making
+"""
+
+from .fusion_engine import FusionEngine, SimpleFusionEngine
+
+__all__ = ['FusionEngine', 'SimpleFusionEngine']
