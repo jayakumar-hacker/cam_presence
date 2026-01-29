77e06ea9ef23d3aa8b364adef680c73d045a4035
Jayakumar K <jayakumar.ethicalhacker@gmail.com>
2026-01-29 09:39:34 -0500

    Create student registration module with CLI
    Initial implementation of the student registration module with CLI support.

---
+++ registration/__init__.py
@@ -0,0 +1,8 @@
+"""
+Student registration module
+CLI-based enrollment system
+"""
+
+from .enroll import StudentEnrollment, run_enrollment_cli
+
+__all__ = ['StudentEnrollment', 'run_enrollment_cli']
