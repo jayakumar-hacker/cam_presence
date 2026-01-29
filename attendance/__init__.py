bafe5b6dcce59074f362c0f0af0cfa0089fef277
Jayakumar K <jayakumar.ethicalhacker@gmail.com>
2026-01-29 09:36:39 -0500

    Initialize attendance module with camera management

---
+++ attendance/__init__.py
@@ -0,0 +1,8 @@
+"""
+Camera management module
+Coordinates multi-camera streams and processing
+"""
+
+from .cam_manager import CameraManager, CameraProcessor
+
+__all__ = ['CameraManager', 'CameraProcessor']
