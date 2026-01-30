9169f47acaf6839d65420af729e8f3ce092e6d73
Jayakumar K <jayakumar.ethicalhacker@gmail.com>
2026-01-30 21:57:21 +0530

    Refactor docstring format in __init__.py

--- attendance/__init__.py
+++ attendance/__init__.py
@@ -3,6 +3,4 @@ Camera management module
 Coordinates multi-camera streams and processing
 """
 
-from .cam_manager import CameraManager, CameraProcessor
 
-__all__ = ['CameraManager', 'CameraProcessor']
