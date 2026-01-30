16cd7b1e60db64758ca96bbe7c60fe87841ab4ce
Jayakumar K <jayakumar.ethicalhacker@gmail.com>
2026-01-30 21:55:48 +0530

    Refactor docstring format in __init__.py

--- detection/__init__.py
+++ detection/__init__.py
@@ -3,6 +3,4 @@ Face detection module
 Multi-face detection using MTCNN and OpenCV
 """
 
-from .face_detector import FaceDetector, BatchFaceDetector
 
-__all__ = ['FaceDetector', 'BatchFaceDetector']
