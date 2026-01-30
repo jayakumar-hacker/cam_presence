da0d1cc4c8177ae3fa07d9ca44b9057d65814733
Jayakumar K <jayakumar.ethicalhacker@gmail.com>
2026-01-30 21:59:08 +0530

    Refactor module docstring format

--- recognition/__init__.py
+++ recognition/__init__.py
@@ -3,7 +3,3 @@ Face recognition module
 Embedding generation and face matching
 """
 
-from .embedding import FaceEmbedder
-from .matcher import FaceMatcher, MultiMatcher
-
-__all__ = ['FaceEmbedder', 'FaceMatcher', 'MultiMatcher']
