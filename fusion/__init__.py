56f027ccc1b4dfd87b83c2fe2aab8f7276a52acf
Jayakumar K <jayakumar.ethicalhacker@gmail.com>
2026-01-30 21:58:44 +0530

    Remove unnecessary lines and retain module docstring

--- fusion/__init__.py
+++ fusion/__init__.py
@@ -3,6 +3,3 @@ Multi-camera fusion module
 Central aggregation and decision making
 """
 
-from .fusion_engine import FusionEngine, SimpleFusionEngine
-
-__all__ = ['FusionEngine', 'SimpleFusionEngine']
