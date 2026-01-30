c7bd07258dabb76fb1159c6ada83294c4d13cbbc
Jayakumar K <jayakumar.ethicalhacker@gmail.com>
2026-01-30 21:59:46 +0530

    Refactor docstring format in __init__.py

--- streams/__init__.py
+++ streams/__init__.py
@@ -3,6 +3,5 @@ Video stream handling module
 Manages RTSP/HTTP camera streams
 """
 
-from .rtsp_handler import StreamHandler, MultiStreamManager
 
-__all__ = ['StreamHandler', 'MultiStreamManager']
+
